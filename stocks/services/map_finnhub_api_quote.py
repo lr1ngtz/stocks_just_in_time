286b2e0f73c04305a22253ddd87fc815c71d4950
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2022-05-24 16:22:29 +0200

    Drop QuoteManager, update map_finnhub_api_quote, create_quote

--- stocks/services/map_finnhub_api_quote.py
+++ stocks/services/map_finnhub_api_quote.py
@@ -1,8 +1,28 @@
 from datetime import datetime
 
+from stocks.models import StockSymbol
 
-def map_finnhub_api_quote(quote_info: dict) -> dict:
+
+def map_finnhub_api_quote(quote_info: dict, stock_symbol: StockSymbol) -> dict:
+    mandatory_keys = ("c", "d", "dp", "h", "l", "o", "pc", "t")
+
+    for key in quote_info:
+        if key not in mandatory_keys:
+            raise Exception()
+
     if quote_info.get("t"):
         quote_info["t"] = datetime.fromtimestamp(quote_info["t"])
 
-    return quote_info
+    mapped_quote_info = {
+        "current_price": quote_info.get("c"),
+        "change": quote_info.get("d"),
+        "percent_change": quote_info.get("dp"),
+        "high_price_of_the_day": quote_info.get("h"),
+        "low_price_of_the_day": quote_info.get("l"),
+        "open_price_of_the_day": quote_info.get("o"),
+        "previous_close_price": quote_info.get("pc"),
+        "timestamp": quote_info.get("t"),
+        "stock_symbol": stock_symbol,
+    }
+
+    return mapped_quote_info
