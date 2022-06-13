67551bcccf7ad82c5c3f01db857f433e1300b180
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2022-06-13 15:15:55 +0200

    Add celery to the project

---
+++ celeryconfig.py
@@ -0,0 +1,8 @@
+broker_url = "redis://localhost:6379/0"
+result_backend = "redis://localhost"
+
+task_serializer = "json"
+result_serializer = "json"
+accept_content = ["json"]
+timezone = "Europe/Warsaw"
+enable_utc = True
