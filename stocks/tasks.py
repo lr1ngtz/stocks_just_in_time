c99a6cb23c6077aec82eecf397992f3e8c4e3f25
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2022-06-17 15:57:18 +0200

    Add celer-beat service, add tasks to admin panel

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -4,15 +4,6 @@ from stocks.services.stock_symbol import create_or_update_stock_symbol
 from stocks.services.quote import create_quote
 
 
-@celery_app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(
-        20,
-        create_quote_task.s("AAPL"),
-        name="scan for expired accounts every 4 hours",
-    )
-
-
 @celery_app.task
 def create_or_update_stock_symbol_task():
     create_or_update_stock_symbol()
